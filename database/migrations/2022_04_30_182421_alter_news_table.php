93fdb2f373a428a8f20c230f9cd8b45a8b50bed8
ElmurodSuyunov01 <elmurodsuyunov02@gmail.com>
2022-04-30 11:32:53 -0700

    migration changed

---
+++ database/migrations/2022_04_30_182421_alter_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->text('body_uz')->change();
+            $table->text('body_oz')->change();
+            $table->text('body_ru')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->string('body_uz')->change();
+            $table->string('body_oz')->change();
+            $table->string('body_ru')->change();
+        });
+    }
+}
